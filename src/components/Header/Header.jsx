e84120a4fb9525c267db14f349d516596f465711
6iller <a.sabitov.msu.moskau@gmail.com>
2025-01-30 15:35:20 +0300

    layout add

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,10 +2,10 @@ import { v4 as uuidv4 } from 'uuid';
 
 export default function Header () {
     const MENU = [
+        {title: 'Home', to: 'Home', key:uuidv4()}, 
+        {title: 'Services', to: 'Services', key:uuidv4()}, 
         {title: 'Portfolio', to: 'portfolio', key:uuidv4()}, 
-        {title: 'Portfolio', to: 'portfolio', key:uuidv4()}, 
-        {title: 'Portfolio', to: 'portfolio', key:uuidv4()}, 
-        {title: 'Portfolio', to: 'portfolio', key:uuidv4()}, 
+        {title: 'About us', to: 'about us', key:uuidv4()}, 
 
     ]
     return (
