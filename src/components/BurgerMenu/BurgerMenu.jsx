f40837f66a13ed6741d994a37d2758b2e548d4ca
6iller <a.sabitov.msu.moskau@gmail.com>
2025-02-07 19:27:42 +0300

    burger

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -5,9 +5,7 @@ import './BurgerMenu.css';
 const BurgerMenu = () => {
   return (
     <div style={{ backgroundColor: 'lightgray', padding: '10px' }}>
-      {/* Здесь будет ваш код для меню */}
       <p>Это скрытое меню!</p>
-      {/* Добавьте здесь элементы вашего меню (ссылки, кнопки и т.д.) */}
     </div>
   );
 };
